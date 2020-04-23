bbcca5dc2ea2fd21f6621206a73aef138609a00d
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 17:00:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    "Hello, my name is #{name}."
+  end 
+end 
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end 
+end 
