acd2c4437d703b718ed8ecae4c4203ee3d5eba97
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 17:00:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end 
 end 
