515960972af21c73d1c2f483784e2aff0f4b204a
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 17:02:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,7 @@ def assign_rooms(attendees)
 end 
 
 def printer(attendees)
-  attendees.batch_badge_creator
-  attendees.assign_rooms
+  
+  batch_badge_creator(attendees).each do |attendee|
+  end 
 end 
